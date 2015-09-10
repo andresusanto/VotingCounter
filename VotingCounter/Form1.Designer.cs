0b29d3a77a010d16b40f9c33da4f92af193585e7
andresusanto <andre@andresusanto.info>
2015-09-10 19:57:19 +0700

    Voting Counter

---
+++ VotingCounter/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace VotingCounter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint1 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 0D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint2 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(2D, 0D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint3 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(5D, 0D);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblCalon5 = new System.Windows.Forms.Label();
+            this.lblCalon3 = new System.Windows.Forms.Label();
+            this.lblCalon1 = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(-30, -14);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            dataPoint1.IsValueShownAsLabel = true;
+            dataPoint1.Label = "Joshua Belzalel";
+            dataPoint2.IsValueShownAsLabel = true;
+            dataPoint2.Label = "Aryya Dwisatya W";
+            dataPoint3.IsValueShownAsLabel = true;
+            dataPoint3.Label = "Abstain";
+            series1.Points.Add(dataPoint1);
+            series1.Points.Add(dataPoint2);
+            series1.Points.Add(dataPoint3);
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(679, 556);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Controls.Add(this.lblCalon5);
+            this.groupBox1.Controls.Add(this.lblCalon3);
+            this.groupBox1.Controls.Add(this.lblCalon1);
+            this.groupBox1.Controls.Add(this.pictureBox5);
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(463, 232);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(342, 273);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Detil Suara";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(179, 148);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(150, 83);
+            this.textBox1.TabIndex = 12;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(179, 237);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(149, 23);
+            this.progressBar1.TabIndex = 10;
+            // 
+            // lblCalon5
+            // 
+            this.lblCalon5.AutoSize = true;
+            this.lblCalon5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCalon5.Location = new System.Drawing.Point(109, 187);
+            this.lblCalon5.Name = "lblCalon5";
+            this.lblCalon5.Size = new System.Drawing.Size(30, 31);
+            this.lblCalon5.TabIndex = 9;
+            this.lblCalon5.Text = "0";
+            // 
+            // lblCalon3
+            // 
+            this.lblCalon3.AutoSize = true;
+            this.lblCalon3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCalon3.Location = new System.Drawing.Point(282, 61);
+            this.lblCalon3.Name = "lblCalon3";
+            this.lblCalon3.Size = new System.Drawing.Size(30, 31);
+            this.lblCalon3.TabIndex = 7;
+            this.lblCalon3.Text = "0";
+            // 
+            // lblCalon1
+            // 
+            this.lblCalon1.AutoSize = true;
+            this.lblCalon1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCalon1.Location = new System.Drawing.Point(109, 62);
+            this.lblCalon1.Name = "lblCalon1";
+            this.lblCalon1.Size = new System.Drawing.Size(30, 31);
+            this.lblCalon1.TabIndex = 5;
+            this.lblCalon1.Text = "0";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::VotingCounter.Properties.Resources.abstain;
+            this.pictureBox5.Location = new System.Drawing.Point(6, 148);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(97, 112);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::VotingCounter.Properties.Resources.v3;
+            this.pictureBox2.Location = new System.Drawing.Point(179, 19);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(97, 112);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::VotingCounter.Properties.Resources.v1;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(97, 112);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(817, 517);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "PEMILU COUNTER";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblCalon5;
+        private System.Windows.Forms.Label lblCalon3;
+        private System.Windows.Forms.Label lblCalon1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ VotingCounter/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using OpenPop.Mime;
+using OpenPop.Mime.Header;
+using OpenPop.Pop3;
+using OpenPop.Pop3.Exceptions;
+using OpenPop.Common.Logging;
+using Message = OpenPop.Mime.Message;
+
+namespace VotingCounter
+{
+    public partial class Form1 : Form
+    {
+        private readonly Dictionary<int, Message> messages = new Dictionary<int, Message>();
+		private readonly Pop3Client pop3Client;
+
+        int[] suara = new int[5];
+        Label[] lblCalon = new Label[5];
+
+        int totalSuara = 0;
+
+        List<string> kupon = new List<string>();
+
+        String passEMAIL;
+
+        public Form1(String pas_)
+        {
+            InitializeComponent();
+            pop3Client = new Pop3Client();
+            System.Windows.Forms.Form.CheckForIllegalCrossThreadCalls = false;
+
+            passEMAIL = pas_;
+
+        }
+
+        private void updateData()
+        {
+            suara[0] = 0;
+            suara[1] = 0;
+            suara[2] = 0;
+            suara[3] = 0;
+            suara[4] = 0;
+
+            lblCalon[0] = lblCalon1;
+            //lblCalon[1] = lblCalon2;
+            lblCalon[2] = lblCalon3;
+            //lblCalon[3] = lblCalon4;
+            lblCalon[4] = lblCalon5;
+
+
+            try
+            {
+                if (pop3Client.Connected)
+                    pop3Client.Disconnect();
+                pop3Client.Connect("pop.gmail.com", 995, true);
+                pop3Client.Authenticate("ronde2pemiluhmif@gmail.com", passEMAIL);
+                int count = pop3Client.GetMessageCount();
+
+                progressBar1.Maximum = count;
+
+                int success = 0;
+                int fail = 0;
+                for (int i = count; i >= 1; i -= 1)
+                {
+                    // Check if the form is closed while we are working. If so, abort
+                    if (IsDisposed)
+                        return;
+
+                    // Refresh the form while fetching emails
+                    // This will fix the "Application is not responding" problem
+                    Application.DoEvents();
+
+                    try
+                    {
+                        Message message = pop3Client.GetMessage(i);
+
+                        String pesan;
+
+                        if (message.MessagePart.IsText)
+                        {
+                            pesan = message.MessagePart.GetBodyAsText();
+                        }
+                        else
+                        {
+                            MessagePart plainTextPart = message.FindFirstPlainTextVersion();
+                            if (plainTextPart != null)
+                            {
+                                pesan = plainTextPart.GetBodyAsText();
+                            }
+                            else
+                            {
+                                // Try to find a body to show in some of the other text versions
+                                List<MessagePart> textVersions = message.FindAllTextVersions();
+                                if (textVersions.Count >= 1)
+                                    pesan = textVersions[0].GetBodyAsText();
+                                else
+                                    pesan = "<<OpenPop>> Cannot find a text version body in this message to show <<OpenPop>>";
+                            }
+                        }
+
+                        if (message.Headers.Subject == "2SUARA KAHIM")
+                        {
+                            //MessageBox.Show("IBM");
+                            String[] komponen = pesan.Split('|');
+                            if (komponen.Count() == 4)
+                            {
+                                if (komponen[0] == "COMBODUO")
+                                {
+                                    if (!kupon.Contains(komponen[2]))
+                                    {
+                                        int pilihan = 0;
+                                        if (Int32.TryParse(komponen[3], out pilihan))
+                                        {
+                                            kupon.Add(komponen[2]);
+                                            textBox1.AppendText(komponen[2] + " " + komponen[3] + "\n");
+
+                                            suara[pilihan - 1] += 1;
+                                            lblCalon[pilihan - 1].Text =  suara[pilihan - 1].ToString();
+                                            chart1.Series.Clear();
+                                            chart1.Series.Add("");
+                                            chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+
+                                            chart1.Series[0].Points.Add(0);
+                                            chart1.Series[0].Points.Add(0);
+                                            chart1.Series[0].Points.Add(0);
+                                            //chart1.Series[0].Points.Add(0);
+                                            //chart1.Series[0].Points.Add(0);
+
+                                            chart1.Series[0].Points[0].IsValueShownAsLabel = true;
+                                            chart1.Series[0].Points[0].Label = "Joshua Belzalel A";
+                                            chart1.Series[0].Points[0].SetValueY(suara[0]);
+
+                                            //chart1.Series[0].Points[1].IsValueShownAsLabel = true;
+                                            //chart1.Series[0].Points[1].Label = "Farid Fadhil H";
+                                            //chart1.Series[0].Points[1].SetValueY(suara[1]);
+
+                                            chart1.Series[0].Points[1].IsValueShownAsLabel = true;
+                                            chart1.Series[0].Points[1].Label = "Aryya Dwisatya W";
+                                            chart1.Series[0].Points[1].SetValueY( suara[2] );
+
+                                            //chart1.Series[0].Points[3].IsValueShownAsLabel = true;
+                                            //chart1.Series[0].Points[3].Label = "Vidia Anindhita";
+                                            //chart1.Series[0].Points[3].SetValueY( suara[3] );
+
+                                            chart1.Series[0].Points[2].IsValueShownAsLabel = true;
+                                            chart1.Series[0].Points[2].Label = "Abstain";
+                                            chart1.Series[0].Points[2].SetValueY( suara[4] );
+
+                                            progressBar1.Value++;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                        //MessageBox.Show(message.Headers.Subject + "\n\n" + pesan);
+
+                        success++;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                        DefaultLogger.Log.LogError(
+                            "TestForm: Message fetching failed: " + ex.Message + "\r\n" +
+                            "Stack trace:\r\n" +
+                            ex.StackTrace);
+                        fail++;
+                    }
+
+                    //progressBar.Value = (int)(((double)(count - i) / count) * 100);
+                }
+
+                //MessageBox.Show(this, "Mail received!\nSuccesses: " + success + "\nFailed: " + fail, "Message fetching done");
+
+                if (fail > 0)
+                {
+                    //MessageBox.Show(this, "");
+                }
+            }
+            catch (InvalidLoginException)
+            {
+                MessageBox.Show(this, "The server did not accept the user credentials!", "POP3 Server Authentication");
+            }
+            catch (PopServerNotFoundException)
+            {
+                MessageBox.Show(this, "The server could not be found", "POP3 Retrieval");
+            }
+            catch (PopServerLockedException)
+            {
+                MessageBox.Show(this, "The mailbox is locked. It might be in use or under maintenance. Are you connected elsewhere?", "POP3 Account Locked");
+            }
+            catch (LoginDelayException)
+            {
+                MessageBox.Show(this, "Login not allowed. Server enforces delay between logins. Have you connected recently?", "POP3 Account Login Delay");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, "Error occurred retrieving mail. " + ex.Message, "POP3 Retrieval");
+            }
+
+            progressBar1.Value = progressBar1.Maximum;
+            MessageBox.Show("Selesai, selamat untuk yang menang :)", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread th = new Thread(updateData);
+            th.Start();
+        }
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
